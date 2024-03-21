0156ad2580b768dcd9c8f5083b3e19ca1c9822e8
SACHINK51 <114231313+SACHINK51@users.noreply.github.com>
2024-03-21 18:32:17 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -2,12 +2,12 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify, flash
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, login_required, UserMixin, login_user, logout_user, current_user
-import mysql.connector
+import mariaDB.connector
 from flask_cors import CORS
 import json
 
-# MySQL database connection
-mysql = mysql.connector.connect(user='web', password='webPass',
+# Maria database connection
+mariaDB = mariaDB.connector.connect(user='web', pwd='webPass',
   host='127.0.0.1',
   database='BookStore')
 
@@ -21,99 +21,99 @@ login_manager.login_view = 'login'
 
 # User class for Flask-Login
 class User(UserMixin):
-    def __init__(self, userID, userName, userType):
-        self.id = userID
-        self.userName = userName
-        self.userType = userType
+    def __init__(self, uID, uName, uType):
+        self.id = uID
+        self.uName = uName
+        self.uType = uType
 
 # Loader function for Flask-Login
 @login_manager.user_loader
-def load_user(userID):
-    user = query_user_by_id(userID)
+def load_user(uID):
+    user = user_by_id_query(uID)
     if user:
         return user
     else:
         return None
 
-# Database query function to get user by ID
-def query_user_by_id(userID):
-    select_query = 'SELECT * FROM users WHERE userID = %s'
-    cursor = mysql.cursor()
-    cursor.execute(select_query, (userID,))
-    user = cursor.fetchone()
+# Function for database query to obtain user by ID
+def user_by_id_query(uID):
+    sel_query = 'SELECT * FROM users WHERE uID = %s'
+    cur = mariaDB.cur()
+    cur.execute(sel_query, (uID,))
+    user = cur.fetchone()
     
     if user:
         return User(user[0], user[1], user[2])
     else:
         return None
 
-# Route for user signup    
+# User signup route    
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     signup_alert = None
     if request.method == 'POST':
-        userName = request.form['userName']
-        userType = request.form['userType']
-        password = request.form['password']
+        uName = request.form['uName']
+        uType = request.form['uType']
+        pwd = request.form['pwd']
 
-        # Hash the password before storing it
-        hashed_password = bcrypt.generate_password_hash(password)
+        # Before storing the password, hash it.
+        hashed_pwd = bcrypt.generate_pwd_hash(pwd)
 
-        # Insert user details into the database
-        insert_query = '''
-            INSERT INTO users (userName, UserType, Password)
+        # Add user information to the database.
+        ins_query = '''
+            INSERT INTO users (uName, uType, pwd)
             VALUES (%s, %s, %s)
         '''
-        cursor = mysql.cursor(); #create a connection to the SQL instance
-        cursor.execute(insert_query, (userName, userType, hashed_password))
-        mysql.commit()
-        flash("Signup successful! Please login.", "success")
-        signup_alert = "Signup successful! Please wait a moment."
+        cur = mariaDB.cur(); #create a connection to the SQL instance
+        cur.execute(ins_query, (uName, uType, hashed_pwd))
+        mariaDB.commit()
+        flash("User registration successful! Please sign in.", "success")
+        signup_alert = "User registration successful! Please wait a moment."
         return render_template('signup.html', signup_alert=signup_alert)
 
     return render_template('signup.html')
 
-# Route for user login	
+# Login Route
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        userName = request.form['userName']
-        userType = request.form['userType']
-        password = request.form['password']
+        uName = request.form['uName']
+        uType = request.form['uType']
+        pwd = request.form['pwd']
 
-        # Check if the user exists and the password is correct
-        select_query = 'SELECT * FROM users WHERE userName = %s AND userType = %s'
-        cursor = mysql.cursor(); #create a connection to the SQL instance
-        cursor.execute(select_query, (userName, userType))
-        user = cursor.fetchone()
+        # Verify that the password is correct and the user exists.
+        sel_query = 'SELECT * FROM users WHERE uName = %s AND uType = %s'
+        cur = mariaDB.cur(); #create a connection to the SQL instance
+        cur.execute(sel_query, (uName, uType))
+        user = cur.fetchone()
         print(user[0],user[1],user[2],user[3])
-        if user and bcrypt.check_password_hash(user[3], password):
+        if user and bcrypt.check_pwd_hash(user[3], pwd):
             # Set user information in the session
-            session['userID'] = user[0]
-            session['userName'] = user[1]
-            session['userType'] = user[2]
+            session['uID'] = user[0]
+            session['uName'] = user[1]
+            session['uType'] = user[2]
 
             login_user(User(user[0], user[1], user[2]))
 
-            if session['userType'] == "Supplier":
-                return redirect(url_for('supplier_dashboard'));
+            if session['uType'] == "Supplier":
+                return redirect(url_for('supp_dashboard'));
             else:
-                return redirect(url_for('customer_dashboard'));
+                return redirect(url_for('cust_dashboard'));
         else:
-            return 'Invalid userName or password'
+            return 'Invalid uName or pwd'
 
     return render_template('login.html')
 
-# Route for customer dashboard
-@app.route("/customer_dashboard")
+# Customer dashboard route
+@app.route("/cust_dashboard")
 @login_required
-def customer_dashboard():
-    if current_user.is_authenticated and current_user.userType == "Customer":
-        cur = mysql.cursor()
-        cur.execute('''SELECT b.*, u.userName FROM Book b JOIN users u ON b.userID = u.userID''')
-        results  = cur.fetchall()
+def cust_dashboard():
+    if current_user.is_authenticated and current_user.uType == "Customer":
+        cur = mariaDB.cur()
+        cur.execute('''SELECT b.*, u.uName FROM Book b JOIN users u ON b.uID = u.uID''')
+        res  = cur.fetchall()
         books = []
-        for row in results :
+        for row in res :
             book = {
                 'bookID': row[0],
                 'BookName': row[1],
@@ -121,23 +121,23 @@ def customer_dashboard():
                 'Rating': row[3],
                 'Quantity': row[4],
                 'BookDescription': row[5],
-                'userName': row[6]
+                'uName': row[6]
             }
             books.append(book)
         return render_template('customer.html', books=books)
     else:
-        return 'Access denied. You are not a customer.'
+        return 'Access refused. You are not a customer.'
 
-# Route for supplier dashboard     
-@app.route("/supplier_dashboard")
+# Supplier dashboard route   
+@app.route("/supp_dashboard")
 @login_required
-def supplier_dashboard():
-    if current_user.is_authenticated and current_user.userType == "Supplier":
-        cur = mysql.cursor()
-        cur.execute('''SELECT * FROM Book WHERE userID = %s''', (session['userID'],))
-        results  = cur.fetchall()
+def supp_dashboard():
+    if current_user.is_authenticated and current_user.uType == "Supplier":
+        cur = mariaDB.cur()
+        cur.execute('''SELECT * FROM Book WHERE uID = %s''', (session['uID'],))
+        res  = cur.fetchall()
         books = []
-        for row in results :
+        for row in res :
             book = {
                 'bookID': row[0],
                 'BookName': row[1],
@@ -145,39 +145,39 @@ def supplier_dashboard():
                 'Rating': row[3],
                 'Quantity': row[4],
                 'BookDescription': row[5],
-                'userID': row[6]
+                'uID': row[6]
             }
             books.append(book)
         return render_template('supplier.html', books=books)
     else:
-        return 'Access denied. You are not a Supplier.'
+        return 'Access refused. You are not a Supplier.'
 
-# Route for user logout
+# User logout route
 @app.route('/logout', methods=['POST'])
 def logout():
     logout_user()
     # Clear session data
-    session.pop('userID', None)
-    session.pop('userName', None)
-    session.pop('userType', None)
+    session.pop('uID', None)
+    session.pop('uName', None)
+    session.pop('uType', None)
     return redirect(url_for('login'))
 
 # Default route
 @app.route("/") 
 def defaultPage():
     if current_user.is_authenticated:
-        if current_user.userType == "Supplier":
-            return redirect(url_for('supplier_dashboard'))
+        if current_user.uType == "Supplier":
+            return redirect(url_for('supp_dashboard'))
         else:
-            return redirect(url_for('customer_dashboard'))
+            return redirect(url_for('cust_dashboard'))
     else:
         return redirect(url_for('login'))
     
-# Route for adding a new product
+# Book insertion route
 @app.route('/add_book', methods=['GET','POST'])
 @login_required
 def add_book():
-    if current_user.userType == "Supplier":
+    if current_user.uType == "Supplier":
         try:
             if request.method == 'POST':
                 bookName = request.form['bookName']
@@ -185,26 +185,26 @@ def add_book():
                 rating = request.form['rating']
                 quantity = request.form['quantity']
                 bookDescription = request.form['bookDescription']
-                userID = session.get('userID')
+                uID = session.get('uID')
                 
-                # Insert product into the Product table
-                insert_query = '''
-                    INSERT INTO Book (bookName, price, rating, quantity, bookDescription, userID)
+                # Insert book into the Book table
+                ins_query = '''
+                    INSERT INTO Book (bookName, price, rating, quantity, bookDescription, uID)
                     VALUES (%s, %s, %s, %s, %s, %s)
                 '''
-                cursor = mysql.cursor()
-                cursor.execute(insert_query, (bookName, price, rating, quantity, bookDescription, userID))
-                mysql.commit()
+                cur = mariaDB.cur()
+                cur.execute(ins_query, (bookName, price, rating, quantity, bookDescription, uID))
+                mariaDB.commit()
 
-            return redirect(url_for('supplier_dashboard'))
+            return redirect(url_for('supp_dashboard'))
         except Exception as e:
             return jsonify({'error': str(e)}), 500
 
-# Route for updating a product
+# Book updation route
 @app.route('/update_book/<int:book_id>', methods=['GET','PUT'])
 @login_required
 def update_book(book_id):
-    if current_user.userType == "Supplier":
+    if current_user.uType == "Supplier":
         try:
             if request.method == 'PUT':
                 data = request.get_json()
@@ -213,63 +213,63 @@ def update_book(book_id):
                 rating = data.get('new_rating')
                 quantity = data.get('new_quantity')
                 bookDescription = data.get('new_book_description')
-                userID = session.get('userID')
+                uID = session.get('uID')
                 bookID = book_id
 
-                # Update product in the Product table
+                # Update book in the Book table
                 update_query = '''
                     UPDATE Book
                     SET bookName = %s, price = %s, rating = %s, quantity = %s, bookDescription = %s
                     WHERE bookID = %s;
                 '''
-                cursor = mysql.cursor()
-                resp = cursor.execute(update_query, (bookName, price, rating, quantity, bookDescription, bookID))
-                mysql.commit()
+                cur = mariaDB.cur()
+                resp = cur.execute(update_query, (bookName, price, rating, quantity, bookDescription, bookID))
+                mariaDB.commit()
 
                 return jsonify({'message': 'Book updated successfully'}), 200
 
         except Exception as e:
             return jsonify({'error': str(e)}), 500
 
-# Route for deleting a book
+# Book deletion route
 @app.route('/delete_book/<int:book_id>', methods=['GET','DELETE'])
 @login_required
 def delete_book(book_id):
-    if current_user.userType == "Supplier":
+    if current_user.uType == "Supplier":
         try:
             if request.method == 'DELETE':
                 delete_query = '''
                     DELETE FROM Book WHERE bookID = %s
                 '''
-                cursor = mysql.cursor()
-                cursor.execute(delete_query, (book_id,))
-                mysql.commit()
+                cur = mariaDB.cur()
+                cur.execute(delete_query, (book_id,))
+                mariaDB.commit()
 
                 return jsonify({'message': 'Book deleted successfully'}), 200
 
         except Exception as e:
             return jsonify({'error': str(e)}), 500
 
-# Route for filtering a product
+# Book filtering route
 @app.route('/filter/<filter_value>')
 @login_required
 def filter_method(filter_value):
-    if current_user.is_authenticated and current_user.userType == "Customer":
-        cursor = mysql.cursor()
+    if current_user.is_authenticated and current_user.uType == "Customer":
+        cur = mariaDB.cur()
         if(filter_value == "priceLTH"):
-            filterQuery='''SELECT b.*, u.userName FROM Book b JOIN users u ON b.userID = u.userID order By price'''
+            filterQuery='''SELECT b.*, u.uName FROM Book b JOIN users u ON b.uID = u.uID order By price'''
         elif(filter_value == "priceHTL"):
-            filterQuery='''SELECT b.*, u.userName FROM Book b JOIN users u ON b.userID = u.userID order By price DESC'''
+            filterQuery='''SELECT b.*, u.uName FROM Book b JOIN users u ON b.uID = u.uID order By price DESC'''
         elif(filter_value == "ratingLTH"):
-            filterQuery='''SELECT b.*, u.userName FROM Book b JOIN users u ON b.userID = u.userID order By rating'''
+            filterQuery='''SELECT b.*, u.uName FROM Book b JOIN users u ON b.uID = u.uID order By rating'''
         elif(filter_value == "ratingHTL"):
-            filterQuery='''SELECT b.*, u.userName FROM Book b JOIN users u ON b.userID = u.userID order By rating DESC'''
+            filterQuery='''SELECT b.*, u.uName FROM Book b JOIN users u ON b.uID = u.uID order By rating DESC'''
         else:
-            filterQuery='''SELECT b.*, u.userName FROM Book b JOIN users u ON b.userID = u.userID'''
-        cursor.execute(filterQuery);
-        results = cursor.fetchall()
+            filterQuery='''SELECT b.*, u.uName FROM Book b JOIN users u ON b.uID = u.uID'''
+        cur.execute(filterQuery);
+        res = cur.fetchall()
         books = []
-        for row in results :
+        for row in res :
             book = {
                 'bookID': row[0],
                 'BookName': row[1],
@@ -277,30 +277,30 @@ def filter_method(filter_value):
                 'Rating': row[3],
                 'Quantity': row[4],
                 'BookDescription': row[5],
-                'userName': row[6]
+                'uName': row[6]
             }
             books.append(book)
         return jsonify(books), 200
     else:
         return 'Access denied. You are not a customer.'
 
-# Route for searching a book    
+# Book searching route    
 @app.route('/search/<search_term>')
 @login_required
 def search_method(search_term):
-    if current_user.is_authenticated and current_user.userType == "Customer":
-        cursor = mysql.cursor()
+    if current_user.is_authenticated and current_user.uType == "Customer":
+        cur = mariaDB.cur()
         query = '''
-        SELECT b.*, u.userName
+        SELECT b.*, u.uName
         FROM Book b
-        JOIN users u ON b.userID = u.userID
+        JOIN users u ON b.uID = u.uID
         WHERE b.bookName LIKE %s
             OR b.bookDescription LIKE %s
         '''
-        cursor.execute(query, ('%'+ search_term + '%', '%' + search_term + '%'))
-        results = cursor.fetchall()
+        cur.execute(query, ('%'+ search_term + '%', '%' + search_term + '%'))
+        res = cur.fetchall()
         books = []
-        for row in results :
+        for row in res :
             book = {
                 'bookID': row[0],
                 'BookName': row[1],
@@ -308,7 +308,7 @@ def search_method(search_term):
                 'Rating': row[3],
                 'Quantity': row[4],
                 'BookDescription': row[5],
-                'userName': row[6]
+                'uName': row[6]
             }
             books.append(book)
         return jsonify(books), 200
