cc8892dad8ee9bd9a7e4954feb12efc0c3531ac3
SACHINK51 <114231313+SACHINK51@users.noreply.github.com>
2024-03-21 18:47:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify, flash
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, login_required, UserMixin, login_user, logout_user, current_user
-import mariaDB.connector
+import mysql.connector
 from flask_cors import CORS
 import json
 
